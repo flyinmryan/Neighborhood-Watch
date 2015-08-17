1b2a06d6e1cd20acf28d8f0d895c9bf9b756b884
Meera <c.meera85@gmail.com>
2015-08-17 09:04:24 -0700

    Adding folder structure

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var path = require('path');
+
+var app = express();
+
+var bodyParser = require("body-parser");
+
+app.use(express.static(path.join(__dirname, './client')));
+//app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+require('./config/mongoose.js');
+
+require('./config/routes.js')(app);
+
+
+app.listen(8000, function() {
+  console.log('cool stuff on: 8000');
+});
