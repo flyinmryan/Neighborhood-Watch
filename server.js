34b0b2dfce1859f6c5669b6a890bd9d67ca9b2a4
Mike Ryan <flyinmryan@gmail.com>
2015-08-17 16:12:50 -0700

    Cool jQuery magic

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ require('./config/mongoose.js');
 require('./config/routes.js')(app);
 
 
-app.listen(8000, function() {
-  console.log('cool stuff on: 8000');
+app.listen(8888, function() {
+  console.log('cool stuff on: 8888');
 });
