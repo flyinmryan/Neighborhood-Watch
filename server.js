65791b81af3ef27f01f738aeceac283a4bb55415
Meera <c.meera85@gmail.com>
2015-08-19 16:29:18 -0700

    added socket chat room

--- server.js
+++ server.js
@@ -15,6 +15,41 @@ require('./config/mongoose.js');
 require('./config/routes.js')(app);
 
 
-app.listen(8008, function() {
-  console.log('cool stuff on: 8008');
+// app.listen(8008, function() {
+//   console.log('cool stuff on: 8008');
+// });
+var socket_id ;
+var users = [];
+var messages =[];
+var server = app.listen(8000, function() {
+ console.log("listening on port 8000");
+})
+var io = require('socket.io').listen(server) 
+io.sockets.on('connection', function (socket) {
+
+	socket_id = socket.id;
+	// socket.on("hello", function(data) {
+	// 	console.log(data.test);
+	// })
+	
+	socket.on('got_new_message',function (data){
+		//console.log(data);
+		users.push({name: data.name, id: socket_id});
+		//messages.push({user_name : data.name , message : data.message});
+		//console.log(users);
+		io.emit("post_new_message", {user: data.name , new_message: data.message})
+	})
+
+ 	socket.on('disconnect', function () {
+ 		//console.log(socket.id);
+ 		var len = users.length;
+ 		var logged_out_user;
+ 		for(var i =0; i<len; i++){
+ 			if(socket.id === users[i].id)
+ 			{
+ 				logged_out_user = users[i].name ;
+ 			}
+ 		}
+    	io.emit('user_left',{name : logged_out_user }); 
+  	});
 });
